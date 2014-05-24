1c3e9f57913d74e523d19b7f35515acdbf382f9d
Logan Braga <braga.logan@gmail.com>
2014-05-25 00:24:47 +0200

    Form jquery validation

--- Website/assets/js/main.js
+++ Website/assets/js/main.js
@@ -1,4 +1,4 @@
-jQuery(document).ready(function ($) {
+$(document).ready(function ($) {
   var links = $('.navbar').find('a');
 
   $('.tooltip-js').tooltip();
@@ -36,4 +36,58 @@ jQuery(document).ready(function ($) {
     var target = $(this).attr('href');
     $('html, body').animate({ scrollTop: $(target).offset().top + 5}, 1200, 'easeInOutQuart');
   });
+
+  // Form validation
+  var validator = $("#contactForm").validate({
+
+    errorElement: "label",
+    wrapper: "td",
+    errorPlacement: function(error, element) {
+      error.insertBefore( element.parent().parent());
+      error.wrap("<tr class='formError'></tr>");
+      $("<td></td>").insertBefore(error);
+    },
+
+    rules: {
+      message :{
+        required : true,
+        minlength : 10,
+        maxlength : 600
+      },
+      email : {
+        email : true,
+        maxlength : 255
+      },
+      name : {
+        required : true,
+        maxlength : 50,
+        minlength : 2
+      }},
+
+    messages: {
+      name: {
+        required: "Veuillez saisir votre nom",
+        minlength: jQuery.format("Votre nom doit faire au moins {0} carac&egrave;res"),
+        maxlength: jQuery.format("Votre nom doit faire moins de {0} caract&egrave;res")
+      },
+      email: {
+        required: "Veuillez saisir votre adresse mail",
+        email: "Veuillez saisir une adresse mail valide",
+        maxlength : jQuery.format("Votre adresse mail doit faire moins de {0} caract&egrave;res")
+      },
+      message: {
+        required: "Veuillez saisir votre message",
+        minlength: jQuery.format("Votre message doit faire au moins {0} caract&egrave;res"),
+        maxlength: jQuery.format("Votre message doit faire moins de {0} caract&egrave;res")
+      }
+    },
+  });
+
+  $('#formReset').click(function() {
+    validator.resetForm();
+  });
+
+  jQuery.validator.prototype.hideErrors = function() {
+    this.addWrapper( this.toHide  ).slideUp(600);
+  }
 });
